d5876fdafa440c8085ce5a134d9d557f9e5f0e5b
dizzysoup <j481936@gmail.com>
2024-07-28 11:34:41 +0000

    將憑證儲存到DB成功

--- PDP/fido2_server/app.py
+++ PDP/fido2_server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request 
+from flask import Flask, jsonify, request
 from fido2.server import Fido2Server
 from fido2.webauthn import CollectedClientData , AttestationObject , AttestedCredentialData , AuthenticatorData
 from enum import Enum
@@ -7,9 +7,19 @@ import base64
 import json
 import jwt
 from datetime import datetime , timedelta , timezone
+import mysql.connector
+from mysql.connector import errorcode
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+
+db_config = {
+    'user': 'user',
+    'password': 'password',
+    'host': '172.18.0.3',
+    'database': 'fido2',
+    'raise_on_warnings': True
+}
 
 # 日誌紀錄器
 logging.basicConfig(level=logging.INFO)
@@ -34,49 +44,52 @@ def custom_serializer(obj):
     raise TypeError(f"Type {type(obj)} not serializable")
 
 # 生成JWT
-def generate_token(state):
+def generate_token(state , username):
     payload = {
-        'state': state , 
-        'exp' : datetime.now(tz=timezone.utc) + timedelta(hours=1)
+        'state': state ,
+        'exp' : datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        'user' : username
     }
     token = jwt.encode(payload , app.secret_key , algorithm='HS256')
-    return token 
+    return token
 
 def verify_token(token):
     try :
         payload = jwt.decode(token , app.secret_key, algorithms=['HS256'])
-        return payload['state']
+        return payload['state'], payload['user']
     except jwt.ExpiredSignatureError:
         return None
     except jwt.InvalidAudienceError:
-        return None 
+        return None
 
-# POST 請求 註冊begin 
+# POST 請求 註冊begin
 @app.route('/register/begin', methods=['POST'])
 def post_data():
     index = 0
     posted_data  = request.get_json()
     username = posted_data["username"]
+
     user = {"id" : str(index).encode("utf-8") , "name" : username }
-    index = index + 1 
+    index = index + 1
     create_options, state = server.register_begin(
-        user ,
-        resident_key_requirement="required",
-        user_verification=uv,
-        authenticator_attachment="cross-platform",
+            user ,
+            resident_key_requirement="required",
+            user_verification=uv,
+            authenticator_attachment="cross-platform",
     )
     app.logger.info("Statue is : %s" , state )
-    token = generate_token(state) 
-    
+    token = generate_token(state, username)
+
     public_key_json = json.dumps(create_options["publicKey"], default=custom_serializer, indent=4)
-    
+
     data = {
         'message': 'success!',
         'public_key': public_key_json,
-        'token' : token 
+        'token' : token
     }
     app.logger.info('Request Data : ' , data )
-    return jsonify(data)
+    return jsonify(data)    
+
 
 # POST 請求 註冊 complete
 @app.route('/register/complete', methods=['POST'])
@@ -86,18 +99,33 @@ def post_complete_data():
     client_data = posted_data["client_data"]
     # app.logger.info('Client_data： %s',client_data)
     attestation_object  = posted_data["attestation_object"]
-    app.logger.info('Attestation_object : %s' , attestation_object )   
+
     token = posted_data['token']
     # app.logger.info("Token: %s" , posted_data['token'] )
-    state = verify_token(token)
-   
+    state , username = verify_token(token)
+    app.logger.info('UserName : %s' , username )
+
+    # checked user
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+
+    CHECKUSER = ("SELECT COUNT(*) FROM users WHERE username = %s " )
+    cursor.execute(CHECKUSER,(username,))
+    (count , ) = cursor.fetchone()
+    if count == 0 :
+        INSERT_USER = ("INSERT INTO users (username, email) VALUES ( %s , %s )")
+        cursor.execute(INSERT_USER, (username,'example@gmail.com',))
+        cnx.commit()
+        app.logger.info("Inserted new users.")
+    
+
     client_data = CollectedClientData.create(
         client_data['type'],
         base64.urlsafe_b64decode(client_data['challenge']),
         client_data['origin'],
         client_data['cross_origin'] == 'True'
     )
-    
+
     credential_data = AttestedCredentialData.create(
         aaguid = base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['aaguid']),
         credential_id = base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['credential_id']),
@@ -106,34 +134,59 @@ def post_complete_data():
             3 :  base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['public_key']['3']),
             -2 : base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['public_key']['-2']),
             -1 : base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['public_key']['-1']),
-            -3 : base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['public_key']['-3']),           
+            -3 : base64.urlsafe_b64decode(attestation_object['auth_data']['credential_data']['public_key']['-3']),
         }
     )
-   
-    
+
+
     auth_data = AuthenticatorData.create(
         base64.urlsafe_b64decode(attestation_object['auth_data']['rp_id_hash']),
         attestation_object['auth_data']['flags'],
         attestation_object['auth_data']['counter'],
         credential_data
     )
-    
+
     attestation_object = AttestationObject.create(
         attestation_object['fmt'],
         auth_data,
         {
-            'sig' : base64.urlsafe_b64decode(attestation_object['att_stmt']['sig']), 
+            'sig' : base64.urlsafe_b64decode(attestation_object['att_stmt']['sig']),
             'alg' : base64.urlsafe_b64decode(attestation_object['att_stmt']['alg']),
             'x5c' : [base64.b64decode(cert) for cert in attestation_object['x5c']]
         }
     )
-    auth_data = server.register_complete(state , client_data , attestation_object) 
-    app.logger.info("Auth_data : %s" , auth_data.credential_data )
-    data = {
-        'message': 'success!',
-        'data': auth_data.credential_data 
-    }
-    return jsonify(data)
+
+    auth_data = server.register_complete(state , client_data , attestation_object)
+    app.logger.info("Auth_data : %s" , auth_data.credential_data)
+    aaguid = str(auth_data.credential_data.aaguid)
+        
+    CHECK_CREDENTIAL = ('SELECT COUNT(*) FROM credentialData where aaguid = %s')
+    cursor.execute(CHECK_CREDENTIAL , (aaguid , ))
+    (count , ) = cursor.fetchone()
+    
+    if count == 0 :
+        
+        
+        public_key_dict = {k: v.hex() if isinstance(v, bytes) else v for k, v in auth_data.credential_data.public_key.items()}
+        public_keys = json.dumps(public_key_dict)
+        app.logger.info("PublicKeys : %s" , public_keys)
+        ADD_CREDENTIAL = ("INSERT INTO credentialData (aaguid , credential_id , public_key,username) VALUES (%s , %s ,%s , %s )")    
+        cursor.execute(ADD_CREDENTIAL , (aaguid, auth_data.credential_data.credential_id.hex() , public_keys , username))
+        cnx.commit()    
+        app.logger.info("Inserted new credential data.")
+
+        data = {
+            'message': 'success!',
+            'data': "Inserted new credential data."
+        }
+        return jsonify(data)
+    else :
+        data = {
+            'message' : 'Failed ! ',
+            'data' : '使用者已被註冊'
+        }
+        app.logger.error("使用者已被註冊")
+        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ PDP/init.sql
@@ -0,0 +1,34 @@
+-- 初始化資料庫和表格
+
+-- 使用或創建資料庫
+CREATE DATABASE IF NOT EXISTS fido2;
+
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON fido2.* TO 'user'@'%';
+
+-- 使用資料庫
+USE fido2;
+
+-- 創建使用者表
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(100),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (username)
+) ;
+
+
+
+-- 創建 credentialData 表
+CREATE TABLE IF NOT EXISTS credentialData (
+  aaguid CHAR(36) PRIMARY KEY,
+  credential_id BLOB NOT NULL,
+  public_key JSON NOT NULL,
+  username VARCHAR(50),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
+) ;
+
+
+
+FLUSH PRIVILEGES;
